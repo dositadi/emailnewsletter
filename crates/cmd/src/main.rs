2d2d691afe280aa5c6f2beb992f72d8e49ab36be
dositadi <ositadinmadivine61@gmail.com>
2026-08-19 04:14:42 +0100

    new fn added

--- crates/cmd/src/main.rs
+++ crates/cmd/src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{App, HttpServer, web};
-use handlers::greet::greet;
+use actix_web::{ App, HttpServer, web };
+use handlers::{ greet::greet, health_checker::health_check_handler };
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -7,9 +7,9 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check_handler))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+        .bind("127.0.0.1:8000")?
+        .run().await
 }
