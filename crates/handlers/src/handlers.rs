1cca998b944efa3f07fdcc6cbddc4e8fc6727021
dositadi <ositadinmadivine61@gmail.com>
2026-08-25 21:08:44 +0100

    dummy

--- crates/handlers/src/handlers.rs
+++ crates/handlers/src/handlers.rs
@@ -1,9 +1,20 @@
-use actix_web::HttpResponse;
+use actix_web::{ HttpResponse, web };
 
 pub async fn health_check_handler() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub async fn subscribe() -> HttpResponse {
-    HttpResponse::BadRequest().finish()
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    pub email: String,
+    pub name: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    if _form.email.is_empty() || _form.name.is_empty() {
+        HttpResponse::BadRequest().finish()
+    } else {
+        HttpResponse::Ok().finish()
+    }
+    
 }
